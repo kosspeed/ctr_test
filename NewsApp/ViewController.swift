455f757905fbaaa7db7b140dc52636f3bf60fd03
Khwan Siricharoenporn <khwan.sir@ascendcorp.com>
2021-06-08 23:14:22 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Khwan Siricharoenporn on 8/6/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
